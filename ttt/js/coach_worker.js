20a261276f1e35d550374aa551c97aec22196d16
Peter McIntyre <contactpeter1@gmail.com>
2017-02-13 08:53:58 +1100

    added default dna opponent

--- ttt/js/coach_worker.js
+++ ttt/js/coach_worker.js
@@ -21,11 +21,12 @@ var pos;
 var result;
 var i;
 var historyMap = {};
+var dna = "k0o9yuhh04xkrdyyztmsltx85cpup5anr7oiwgtcdz8t8yza9c7nh6scdcxenpczepwt4abhufw6b38y4v3718rzrfuewnhl8ejndil142twwzvsr2arxkrsyo8pqf1j36ahju5ovkcfmbtkve1u0nud01w3fojc4arkfh3c4nd91ohpq7pomxnibrik391ieicrywz50a5fqoqpzqggrkl1d6huc0s4ge72hog8hibsgpzjuz30irerr37780ik5ut2183ttyu4keu1kdg8uk5t6j3972gna0us1p0nb7016usnoko14zvz4r71tkejww03tul873786c0hi9oszqn1haf3xk6fozs2cq9ytmosqqnedfzoyxjivx1tezh4a6pvfpficp7wyvklt81n6097wa16wot9ze6cieuwub0i86qutvu5k0qs0vnt2unury3wtr21ff2j40w4sbjbjaxrlpcpe62ob6ec35gcmr3ojmlnoxapmr2gqtipvaqf6j9k2fsiy269zx106jjnq7atcm77o6p0g60mxose51msc42tuvuslgpqpmpjlujo56wouhjukxn0bguau7g5j3jwxctut6odipxcl2"
 
 onmessage = function(e) {
 
     player = new Neuro( e.data.dna );
-    opponent = e.data.opponent ? new Neuro(e.data.opponent) : new Rando();
+    opponent = e.data.opponent ? new Neuro( e.data.opponent ) : new Rando();
 
     scorecard = {
         score: 0,
